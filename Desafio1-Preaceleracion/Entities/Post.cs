5b96996c9e4f8682b65fede5d3bff967439c6f88
maxi758 <maxi758@gmail.com>
2021-08-30 21:51:53 -0300

    finalizado

---
+++ Desafio1-Preaceleracion/Entities/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio1_Preaceleracion.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Content { get; set; }
+        public User User { get; set; }
+    }
+}
